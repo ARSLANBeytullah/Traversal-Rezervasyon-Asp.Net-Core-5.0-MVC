0495984c31010c32a6324ea4914c2511e90ad82a
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-18 23:18:28 +0300

    Created Models Feature,Feature2,Newsletter,SubAbout,Testimonial

---
+++ EntityLayer/Concrete/Feature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Feature   //Öne çıkan kısmı burada tanımlıyorum. Büyük fotoğraf için tanımladım.Küçük fotolar için feature2 class'ı oluşturdum.Bu kısım revize edilebilir.
+    {
+        public int FeatureID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Testimonial  //Kullancılar bizim hakkımız da ne diyor kısmı için tanımlanan model.
+    {
+        public int TestimonialID { get; set; }
+        public string Client { get; set; }
+        public string Comment { get; set; }
+        public string ClientImage { get; set; }
+        public bool Status { get; set; }
+    }
+}
