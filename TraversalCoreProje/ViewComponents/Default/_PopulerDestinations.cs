78d66045d7628fe6292cd3b96fd85dfcaded2491
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-20 23:22:30 +0300

    Create DestinationManager,_PopulerDestination,Index.cshtml,Default.cshtml

---
+++ TraversalCoreProje/ViewComponents/Default/_PopulerDestinations.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.ViewComponents.Default
+{
+    public class _PopulerDestinations : ViewComponent
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal()); //Refaktoring yapılacak ve EntityFramework'e bapımlılık sonradan giderilecektir.
+        public IViewComponentResult Invoke()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+    }
+}
