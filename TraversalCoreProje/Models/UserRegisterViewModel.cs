167a27b161c818384f4a37c3b4844b69f593648a
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-25 00:55:13 +0300

    Created UserRegisterViewModel,Modified SignIn,SignUp.cshtml,LoginController

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage = "Lütfen adınızı giriniz")] //Name alanının Boş geçilmemesi gerekmesini sağlar.
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen soyadınızı giriniz")]  
+        public string Surname { get; set; }
+        [Required(ErrorMessage = "Lütfen kullanıcı adını giriniz")]  
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen mail adresinizi giriniz")]  
+        public string Mail { get; set; }
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]  
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Lütfen şifreyi tekrar giriniz")] //Name alanının Boş geçilmemesi gerekmesini sağlar.
+        [Compare("Password", ErrorMessage ="Şifreler uyumlu değil")]
+        public string ConfirmPassword { get; set; }
+    }
+}
