8cc4280c57bd009f163f57df083cc5e80f028d73
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-24 23:30:58 +0300

    Created LoginController,SignIn.cshtml,SignUp.cshtml

---
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class LoginController : Controller
+    {
+        [AllowAnonymous] //Altında bulunduğu bütün kodları geçerli bütün kurallardan muaf hala getirir.
+        
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult SignUp()
+        //{
+        //    return View();
+        //}
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+    }
+}
