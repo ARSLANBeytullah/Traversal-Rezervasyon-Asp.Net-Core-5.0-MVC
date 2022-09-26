292e6afe3d13ae6d6b1939340fd403424d8a8c21
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-26 17:54:09 +0300

    Create IReservationService,ReservationManager,IReservationDal,EfReservationDal.Modified ReservationController,NewReservation

--- TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
+++ TraversalCoreProje/Areas/Member/Controllers/ReservationController.cs
@@ -1,5 +1,8 @@
-﻿using EntityLayer.Concrete;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +13,8 @@ namespace TraversalCoreProje.Areas.Member.Controllers
     [Area("Member")]
     public class ReservationController : Controller
     {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal()); //Code Refactoring yapılacak.
+        ReservationManager reservationManager = new ReservationManager(new EfReservationDal());
         public IActionResult MyCurrentReservation()
         {
             return View();
@@ -23,13 +28,23 @@ namespace TraversalCoreProje.Areas.Member.Controllers
         [HttpGet]
         public IActionResult NewReservation()
         {
+            List<SelectListItem> values = (from x in destinationManager.TGetList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.City,
+                                               Value = x.DestinationID.ToString()
+                                           }
+                                           ).ToList();
+            ViewBag.v = values;
             return View();
         }
 
         [HttpPost]
         public IActionResult NewReservation(Reservation p)
         {
-            return View();
+            p.AppUserId = 3;
+            reservationManager.TAdd(p);
+            return RedirectToAction("MyCurrentReservation");
         }
 
         
