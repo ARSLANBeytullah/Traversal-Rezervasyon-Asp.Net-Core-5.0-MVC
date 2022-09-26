db71158987aa1489112f877f576903a24338177d
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-26 13:56:02 +0300

    Created CustomIdentityValidator, Modified SıgnUp.cshtml, Startup.cs

--- TraversalCoreProje/Startup.cs
+++ TraversalCoreProje/Startup.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TraversalCoreProje.Models;
 
 namespace TraversalCoreProje
 {
@@ -28,7 +29,8 @@ namespace TraversalCoreProje
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<Context>();
-            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>(); //Identity yapýlanmasý tanýmlandý
+            services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<Context>()
+            .AddErrorDescriber<CustomIdentityValidator>().AddEntityFrameworkStores<Context>(); //Identity yapýlanmasý tanýmlandý
             services.AddControllersWithViews();
 
             services.AddMvc(config =>
