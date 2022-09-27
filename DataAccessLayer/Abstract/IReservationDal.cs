70fcf03580fd885e7fb63dc87776c7d3e940c7f6
ARSLANBeytullah <arslanbeytullah5800@gmail.com>
2022-09-27 22:36:44 +0300

    Modified Destination.cs,Reservation.cs,IReservationDal,EfReservationDal

--- DataAccessLayer/Abstract/IReservationDal.cs
+++ DataAccessLayer/Abstract/IReservationDal.cs
@@ -9,5 +9,8 @@ namespace DataAccessLayer.Abstract
 {
     public interface IReservationDal : IGenericDal<Reservation>
     {
+        List<Reservation> GetListWithReservationByWaitApproval(int id); //Onay Bekleyen Rezervasyon için tanımlanan method imzası 
+        List<Reservation> GetListWithReservationByAccepted(int id); //Onaylanmış Rezervasyon için tanımlanan method imzası 
+        List<Reservation> GetListWithReservationByPrevious(int id); // Önceki Rezervasyon için tanımlanan method imzası 
     }
 }
